bb2e2a270621339b0263b2e4143f1e46026b96da
Destiny <79973842+destinyiop09@users.noreply.github.com>
2026-09-05 23:11:51 +0000

    updated the main.py

--- src/main.py
+++ src/main.py
@@ -12,12 +12,13 @@ app = FastAPI()
 # Allow the frontend to communicate with the backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-    ],
+ allow_origins=[
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://personal-expense-tracker-pink-two.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
